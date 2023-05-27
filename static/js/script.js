6c0a4a82f193df47d931913a31750159bffec5c2
Mantresh Khurana <mantreshkhurana@gmail.com>
2023-05-27 20:10:49 +0530

    `Theme Handling` Done

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,31 @@
 const toggleSwitch = document.querySelector('#toggle');
 const body = document.querySelector('body');
 
-function toggleTheme() {
-  if (toggleSwitch.checked) {
+function setTheme(theme) {
+  if (theme === 'dark') {
     body.classList.add('dark');
     body.classList.remove('light');
+    toggleSwitch.checked = true;
   } else {
     body.classList.add('light');
     body.classList.remove('dark');
+    toggleSwitch.checked = false;
+  }
+}
+
+function toggleTheme() {
+  if (toggleSwitch.checked) {
+    setTheme('dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    setTheme('light');
+    localStorage.setItem('theme', 'light');
   }
 }
 
 toggleSwitch.addEventListener('change', toggleTheme);
 
+const storedTheme = localStorage.getItem('theme');
+if (storedTheme) {
+  setTheme(storedTheme);
+}
